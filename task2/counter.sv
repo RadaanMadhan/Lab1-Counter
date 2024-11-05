990a6d8e3baceaacfeb4795497782caf9a05797d
RadaanMadhan <madhanradaan@gmail.com>
2024-11-05 21:18:29 +0100

    Finished Lab 1

---
+++ task2/counter.sv
@@ -0,0 +1,18 @@
+module counter #(
+    parameter WIDTH = 8 
+)(
+    //Interface Signals
+    input logic              clk,   //clock
+    input logic              rst,   //reset        
+    input logic              en,    //enable
+    output logic [WIDTH-1:0] count  //count output
+);
+    
+always_ff @(posedge clk ) 
+    if (rst) count <= {WIDTH{1'b0}};
+    //else     count <= count + {{WIDTH-1{1'b1}}, en};
+    else     count <= count + {{WIDTH-1{en}}, 1'b1};
+
+
+endmodule
+
